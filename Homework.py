8641da53f52cd1345c5501fea6c723f3b43961d3
PicklePuff13 <puffkinpickle@gmail.com>
2024-11-12 18:42:15 +0000

    Update Homework.py

--- Homework.py
+++ Homework.py
@@ -1,20 +1,19 @@
-import pgzrun, random, pygame
+from turtle import Screen
+import pgzrun, random
 
 WIDTH=700
 HEIGHT=700
 
 def draw():
     screen.fill("black")
-    width=WIDTH
-    height=HEIGHT-200
-    radius = 10
+    radius=350
     for i in range(40):
         blue=random.randint(0,255)
         red=random.randint(0,255)
         green=random.randint(0,255)
-        pygame.draw.circle(window, (red,green,blue), 0, radius, 2) #(r, g, b) is color, (x, y) is center, R is radius and w is the thickness of the circle border
-        
-        radius#=10
+        screen.draw.circle((red,green,blue), (350,350), radius)
+        radius-=10
+       # screen.draw.circle
 
 def update():
     pass

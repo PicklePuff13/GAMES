5f562f848b48a4f73529f5f0b1353b53a371706d
PicklePuff13 <puffkinpickle@gmail.com>
2024-11-26 18:20:00 +0000

    Update PickleGame.py

--- Pickle Game/PickleGame.py
+++ Pickle Game/PickleGame.py
@@ -3,6 +3,7 @@ import pgzrun, random
 WIDTH=700
 HEIGHT=700
 
+Score=0
 Message="Pickle Clicker"
 Pickle=Actor("pickle character.png")
 
@@ -11,6 +12,7 @@ def draw():
     screen.fill("light green")
     Pickle.draw()
     screen.draw.text(Message, center=(350,100), fontsize=35)
+    screen.draw.text(str(Score), center=(75, 625), fontsize=50)
 
 #random position for the pickle
 def position():
@@ -22,15 +24,17 @@ def update():
     pass
 
 def on_mouse_down(pos):
-    global Message
+    global Message, Score
     if Pickle.collidepoint(pos):
         Message="You caught me!"
+        Score=(Score+1)
         position()
     else:
         Message="You missed!"
+        Score=(Score-1)
 
 position()
 
-clock.schedule_interval(position, 1)
+clock.schedule_interval(position, 2.5)
 
 pgzrun.go()

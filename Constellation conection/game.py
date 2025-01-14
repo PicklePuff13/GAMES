02f724280312280b42ae8933c8334a65f38f8add
PicklePuff13 <puffkinpickle@gmail.com>
2025-01-14 19:00:09 +0000

    Update game.py

--- Constellation conection/game.py
+++ Constellation conection/game.py
@@ -1,6 +1,6 @@
 import random, pgzrun, time
 
-WIDTH=1000
+WIDTH=700
 HEIGHT=700
 
 #variables
@@ -13,19 +13,39 @@ Middle_Time= 0
 End_Time= 0
 
 def starmaker():
+    global Start_Time
     for i in range(8):
         Star=Actor("star.png")
-        Star.pos=random.randint(650,50),random.randint(650,50)
-        Stars.apend(Star)
+        Star.pos=random.randint(50,650),random.randint(50,650)
+        Stars.append(Star)
     Start_Time=time.time()
 
 def draw():
+    global End_Time
     screen.blit("galaxy bg.jpg",(0,0))
+    number=1
     for a in Stars:
         a.draw()
+        screen.draw.text(str(number),(a.pos[0],a.pos[1]))
+        number+=1
+    for i in Lines:
+        screen.draw.line(i[0],i[1],"white")
+    if Next_Star<Total_Stars:
+        End_Time=time.time()-Start_Time
+        screen.draw.text(str(round(End_Time,1)),(50,50))
+    else:
+        screen.draw.text(str(round(End_Time,1)),(50,50))
 
 def update():
     pass
+
+def on_mouse_down(pos):
+    global Next_Star,Lines
+    if Next_Star<Total_Stars:
+        if Stars[Next_Star].collidepoint(pos):
+            if Next_Star:
+                Lines.append((Stars[Next_Star-1].pos,Stars[Next_Star].pos))
+            Next_Star+=1
 
 starmaker()
 

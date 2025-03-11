ebaf131f5172f8d00fa3b37f0b40a70cf7a15d94
PicklePuff13 <puffkinpickle@gmail.com>
2025-03-11 18:54:25 +0000

    Update Shootergame.py

--- Zombie Shooter/Shootergame.py
+++ Zombie Shooter/Shootergame.py
@@ -10,14 +10,29 @@ player=Actor("player.png")
 player.pos=(420,530)
 enemy=Actor("zombieenemy.png")
 enemy.pos=(425,39)
+youwin=False
+win=""
 
 def draw():
-    screen.blit("backround.jpg", (0,0))
-    player.draw()
-    enemy.draw()
-    screen.draw.text("player's score = "+str(playerscore),(20,20),color="black")
-    for i in bulletlist:
-      i.draw()
+    if youwin:
+        screen.blit("youwin.jpg",(0,0))
+        if win=="enemy":
+            screen.draw.text(str(win)+" wins",(300,450),color="red", fontsize=60)
+        elif win=="player":
+            screen.draw.text(str(win)+" wins",(300,450),color="darkgreen", fontsize=60)    
+    else:        
+        screen.blit("backround.jpg", (0,0))
+        player.draw()
+        enemy.draw()
+        screen.draw.text("player's score = "+str(playerscore),(20,20),color="darkgreen")
+        screen.draw.text("enemy's score = "+str(enemyscore),(680,20),color="red")
+        for i in bulletlist:
+         i.draw()
+
+def Youwin(winner):
+    global youwin, win
+    youwin=True
+    win=winner
 
 #space key for the bullet
 def on_key_down(key):
@@ -27,9 +42,10 @@ def on_key_down(key):
         bulletlist[-1].y=player.y-30
     
 def update():
-    global playerscore
+    global playerscore, enemyscore
     enemy.y+=2
     if enemy.y>=560:
+        enemyscore=enemyscore+1
         enemy.y=0
         enemy.x=random.randint(20,830)
     if keyboard.left:
@@ -55,5 +71,9 @@ def update():
             enemy.y=0
             enemy.x=random.randint(20,830)
             playerscore=playerscore+1
+    if playerscore==20:
+        Youwin("player")
+    if enemyscore==15:
+        Youwin("enemy")
 
 pgzrun.go()

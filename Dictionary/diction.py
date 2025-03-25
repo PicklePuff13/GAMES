4306a726c7fd99434073e9dff151ccfc89c5d072
PicklePuff13 <puffkinpickle@gmail.com>
2025-03-25 18:23:31 +0000

    Add files via upload

---
+++ Dictionary/diction.py
@@ -0,0 +1,10 @@
+#creating dictionary
+Pickle={"Name":"person","Age":11,"Hobby":"Art"}
+print(Pickle["Hobby"])
+
+#length of dictionary
+print(len(Pickle))
+
+#adding new item to dictionary
+Pickle["Colour"]="Green"
+print(Pickle)

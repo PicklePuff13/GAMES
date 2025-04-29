77686dfa98849ea7b23c4f3bed7c35d4e949e714
PicklePuff13 <puffkinpickle@gmail.com>
2025-04-29 18:55:13 +0100

    Update diction.py

--- Dictionary/diction.py
+++ Dictionary/diction.py
@@ -1,6 +1,7 @@
 #creating dictionary
-Pickle={"Name":"person","Age":11,"Hobby":"Art"}
+Pickle={"Name":"person","Age":11,"Hobby":"Art","Age":56}
 print(Pickle["Hobby"])
+print(Pickle)
 
 #length of dictionary
 print(len(Pickle))
@@ -8,3 +9,19 @@ print(len(Pickle))
 #adding new item to dictionary
 Pickle["Colour"]="Green"
 print(Pickle)
+
+#accesing items
+print(Pickle["Hobby"])
+print(Pickle["Age"])
+
+#modifying existing item
+Pickle["Name"]="Eva"
+print(Pickle)
+
+#removing an item
+del Pickle["Hobby"]
+print(Pickle)
+
+#dictionary methods
+print (Pickle.values())
+print (Pickle.keys())

a9a269f3c32bfc65b1c3464be826bc85e55841a1
PicklePuff13 <puffkinpickle@gmail.com>
2025-05-13 18:27:34 +0100

    Update Racing.py

--- Racing Game/Racing.py
+++ Racing Game/Racing.py
@@ -8,14 +8,33 @@ Bear=Actor("bear.png",pos=(100,450))
 Fox=Actor("fox.png",pos=(100,300))
 Tiger=Actor("tiger.png",pos=(100,150))
 Animals=[Rabbit,Bear,Fox,Tiger]
+AnimalsS=[]
+
+#random speed for all animals
+for a in Animals:
+    speed=random.randint(2,7)
+    AnimalsS.append (speed)
+
+winner=None
+gameover=False
 
 def draw():
     screen.blit("junglebackdrop.jpg",(0,0))
     for i in Animals:
         i.draw()
     screen.draw.line((830,0),(830,750),"white")
+    if gameover and winner:
+        screen.draw.text("winner is "+str(winner),center=(425,30),fontsize=40,color="white")
 
 def update():
     pass
+    global winner, gameover
+    for i in range(len(Animals)):
+        Animals[i].x+=AnimalsS[i]
+        if Animals[i].x>=830:
+            winner=Animals[i].image.capitalize()
+            gameover=True
+            break
+
 
 pgzrun.go()
